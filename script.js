ff0e3fd6e40adc6d031fd9fd7d65e1d7a1b6973a
kikodrex <50791270+kikodrex@users.noreply.github.com>
2025-07-28 14:45:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ function closeModal() {
   document.getElementById("orderModal").style.display = "none";
 }
 
-// Countdown Timer
-const countdownDate = new Date().getTime() + 86400000; // 24 hrs from now
-const x = setInterval(function () {
+const countdownDate = new Date().getTime() + 86400000;
+const x = setInterval(() => {
   const now = new Date().getTime();
   const distance = countdownDate - now;
   const hrs = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const secs = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("timer").innerHTML = hrs + "h " + mins + "m " + secs + "s ";
+  document.getElementById("timer").innerHTML = `${hrs}h ${mins}m ${secs}s`;
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("timer").innerHTML = "EXPIRED";
